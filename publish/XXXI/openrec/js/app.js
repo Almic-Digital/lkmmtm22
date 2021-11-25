5943cb10d12c3a0d922eb994983e305a45e7e1ad
michaelfrancesco <michael.francesco12@gmail.com>
2021-11-25 20:37:53 +0700

    Set peraturan

--- publish/XXXI/openrec/js/app.js
+++ publish/XXXI/openrec/js/app.js
@@ -50,28 +50,28 @@ $(document).ready(function () {
         var nrp = $("#nrp").val();
         var pass = $("#password").val();
 
-if(parseInt(nrp.substring(3,5)) > 20 ){
+        if (parseInt(nrp.substring(3, 5)) > 20) {
             alert("Acara ini diselenggarakan untuk angkatan 2020 keatas");
-        }else{
-$.ajax({
-            method: 'POST',
-            url: "api/submitnrp.php",
-            data: { nrp: nrp, pass: pass },
-            dataType: 'json',
-            success: function (response) {
-                
-                if (response['status'] == 1) {
-                    alert(response['error']);
-                    window.location = response['redirect'];
-                } else {
-                    alert(response['error']);
-                }
-            }, error: function (e) {
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: "api/submitnrp.php",
+                data: { nrp: nrp, pass: pass },
+                dataType: 'json',
+                success: function (response) {
 
-            }
-        });
-}
-        
+                    if (response['status'] == 1) {
+                        alert(response['error']);
+                        window.location = response['redirect'];
+                    } else {
+                        alert(response['error']);
+                    }
+                }, error: function (e) {
+
+                }
+            });
+        }
+
     });
 
     //Login Form
@@ -80,8 +80,8 @@ $.ajax({
         nrp = nrp.toLowerCase();
         var password = $("#password").val();
         var captcha = grecaptcha.getResponse();
-        
-        
+
+
 
         function loadingButton() {
             $("#login-button").append('<span id="spinner" class="spinner-grow spinner-grow-sm ml-3" role="status" aria-hidden="true"></span>');
@@ -104,7 +104,7 @@ $.ajax({
                 } else {
                     alert(response['error']);
                     window.location = response['redirect'];
-                    
+
                 }
 
             },
@@ -116,8 +116,6 @@ $.ajax({
     });
 
 
-
-
     function data_check() {
         $.ajax({
             method: 'GET',
@@ -125,19 +123,41 @@ $.ajax({
             success: function (status) {
                 $('.navbar').html('');
                 loadnavbar();
-                
 
-                if(status['inform']==0){
-                    var newData = "<h1 class='h1-message'>Thank You for <br> Your Participation!</h1><p class='p-message ml-2'>Please contact our committe for your schedule <br>Download Form Surat Izin Orang Tua : <a href=\"https://petra.id/FormatSuratIzinLKMMTMXXXI\">Link Surat</a><br><br>*)Mohon form suat dapat dikumpulkan <a href=\"https://petra.id/PengumpulanSuratIzinLKMMTMXXXI\">disini</a> </p> <br><br> <p class='p-message'> <b>Best Regards,</b> <br>"+ status['alias']+"</p> " ;
-                    $(".card2").html('');
-                    $(".card2").append(newData);
+
+                if (status['inform'] == 0) {
+                    var newData = "<h1 class='h1-message'>Thank You for <br> Your Participation!</h1><p class='p-message ml-2'>Please contact our committe for your schedule <br>Download Form Surat Izin Orang Tua : <a href=\"https://petra.id/FormatSuratIzinLKMMTMXXXI\">Link Surat</a><br><br>*)Mohon form suat dapat dikumpulkan <a href=\"https://petra.id/PengumpulanSuratIzinLKMMTMXXXI\">disini</a> </p> <br><br> <p class='p-message'> <b>Best Regards,</b> <br>" + status['alias'] + "</p> ";
+                    $("#main-card .card2").html('');
+                    $("#main-card .card2").append(newData);
                 }
                 else if (status['status'] == 1) {
-                    var newData = "<h1 class='h1-message'>Thank You for <br> Your Participation!</h1><p class='p-message ml-2'>Your Schedule will be held at : <br><br>Date: " + status['hari'] + " <br>Time: " + status['jam'] + "<br>Place: <a href=\"" + status['place'] + "\">"+status['place']+"</a><br>Download Form Surat Izin Orang Tua : <a href=\"https://petra.id/FormatSuratIzinLKMMTMXXXI\">Link Surat</a><br><br>*)Mohon form suat dapat dikumpulkan <a href=\"https://petra.id/PengumpulanSuratIzinLKMMTMXXXI\">disini</a> </p> <br><br> <p class='p-message'> <b>Best Regards,</b> <br>"+ status['alias']+"</p> " ;
-                    $(".card2").html('');
-                    $(".card2").append(newData);
+                    var newData = "<h1 class='h1-message'>Thank You for <br> Your Participation!</h1><p class='p-message ml-2'>Your Schedule will be held at : <br><br>Date: " + status['hari'] + " <br>Time: " + status['jam'] + "<br>Place: <a href=\"" + status['place'] + "\">" + status['place'] + "</a><br>Download Form Surat Izin Orang Tua : <a href=\"https://petra.id/FormatSuratIzinLKMMTMXXXI\">Link Surat</a><br><br>*)Mohon form suat dapat dikumpulkan <a href=\"https://petra.id/PengumpulanSuratIzinLKMMTMXXXI\">disini</a> </p> <br><br> <p class='p-message'> <b>Best Regards,</b> <br>" + status['alias'] + "</p> ";
+                    $("#main-card .card2").html('');
+                    $("#main-card .card2").append(newData);
 
                 }
+                var Peraturan = `
+                <div id="second-card" class="card1">
+                        <div class="card2 text-start">
+                        <h1 class="h1-message">
+                            SOP PENGISIAN JADWAL WAWANCARA PESERTA LKMM-TM XXXI 
+                        </h1>
+                        <ol>
+                            <li>Pengisian jadwal wawancara hanya dapat diisi satu kali saja dan hanya pada jadwal wawancara yang tersedia </li>
+                            <li>Batas pengisian adalah H-1 dari tanggal wawancara yang dipilih (Maksimal jam 22.00 WIB)</li>
+                            <li>Semua jam yang tertera dalam daftar merupakan jam menurut Waktu Indonesia Barat (WIB)</li>
+                            <li>Diharap untuk MENCATAT id line contact person dan link google meet karena hanya akan diinfokan sekali saja</li>
+                            <li>Segala bentuk pertanyaan mengenai Wawancara LKMM-TM XXXI, dapat langsung menghubungi Contact Person yang tersedia</li>
+                            <li>WAJIB join ke link google meet yang telah disediakan 10 menit sebelum wawancara dimulai</li>
+                            <li>Peserta diharapkan berpakaian sopan dan rapi untuk wawancara (pakaian standar universitas)</li>
+                            <li>Batas keterlambatan adalah 10 menit dari jadwal yang telah ditentukan</li>
+                            <li>Peserta diharapkan dapat memperhatikan segala ketentuan dan peraturan Wawancara LKMM-TM XXXI  yang berlaku</li>
+                            <li>Jika terdapat kendala saat mengisi jadwal wawancara, dapat langsung menghubungi Official Account (OA) Line LKMM-TM : @210qiwnm</li>
+                        </ol> 
+                        </div>
+                    </div>
+                `;
+                $("#container-peraturan").append(Peraturan)
 
 
             }, error: function (status) {
@@ -153,21 +173,21 @@ $.ajax({
             data: { x: 'y' },
             dataType: 'json',
             success: function (response) {
-                
+
                 if (response == 'false') {
                     window.location = 'login.html';
-                    
+
                 } else if (response == 'true') {
-                    
+
                     $('#no').hide();
                     $('.dash').show();
                 } else {
                     window.location = 'login.html';
-                    
+
                 }
             }, error: function (e) {
                 window.location = 'login.html';
-                
+
             }
         });
     }
@@ -180,7 +200,7 @@ $.ajax({
             success: function (data) {
 
                 data.forEach(function (i) {
-                    
+
                     var option = "<option value='" + i['id'] + "'>" + i['alias'] + " / " + i['hari'] + " / " + i['jadwal'] + "</option>";
 
                     $("#jadwal").append(option);
@@ -200,7 +220,7 @@ $.ajax({
             $("#submit-interview").append('<span id="spinner" class="spinner-grow spinner-grow-sm ml-3" role="status" aria-hidden="true"></span>');
         };
         var jadwal = $("#jadwal").val();
-        
+
         if (jadwal != ' ') {
             $.ajax({
                 url: 'api/pilihwawancara.php',
@@ -211,7 +231,7 @@ $.ajax({
                 },
                 success: function (data) {
                     $("#spinner").remove();
-                    
+
                     if (data['status'] == 0) {
                         alert(data['error']);
                     } else {
@@ -232,7 +252,7 @@ $.ajax({
         data_check();
         load_interview();
 
-       
+
     }
 
     // loadnavbar();
